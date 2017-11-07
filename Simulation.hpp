917610454e16c7ca86c70d245bbf9aeab067094a
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-07 09:14:14 +0100

    Skeleton of the simulation class

---
+++ Simulation.hpp
@@ -0,0 +1,53 @@
+#ifndef SIMULATION_HPP
+#define SIMULATION_HPP
+
+#include <vector>
+
+
+/**
+* The class Simulation manages the user defined parameters and launches the experiment
+*/
+
+class Simulation
+{
+	public:
+		/**
+		 * Constructor
+		 * creates a new population with the values decided by the user
+		 */
+		Simulation();
+		
+		/**
+		 * @return the number of time the user wants to run the program with the same parameters
+		 */
+		unsigned int runsNumber();
+		
+		/**
+		 * @return the population size according to the user's desire
+		 */
+		unsigned int populationSize();
+		
+		/**
+		 * @return the number of generation according to the user's desire
+		 */
+		unsigned int generationsNumber();
+		
+		/**
+		 * @return the number of alleles according to the user's desire
+		 * The user can provide either a number of alleles or a files containing the alleles -> fasta format
+		 */
+		unsigned int allelesNumber();
+		
+		/**
+		 * @return the frequency of each alleles
+		 */
+		std::vector<double> allelesFrequency(unsigned int alleles_number);
+		
+		
+		
+
+
+};
+
+#endif
+
