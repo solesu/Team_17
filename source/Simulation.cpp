7d2b301fc1e4a834b3eeff9e7dc93e56c44e66d0
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-14 10:36:46 +0100

    Modification Simulation.cpp

--- source/Simulation.cpp
+++ source/Simulation.cpp
@@ -1,7 +1,7 @@
 #include "Simulation.hpp"
 #include <iostream>
 
-void Simulation::launchSimulation(Population p, unsigned int runs_number)
+void Simulation::launchSimulation(Population* p, unsigned int runs_number)
 { 
 	for(unsigned int i(0); i<runs_number; ++i) //Will run the simulation with the same parameters runs_number times
 	{
