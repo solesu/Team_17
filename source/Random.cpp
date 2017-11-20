c3972ee35515ee0851663e366f70a5ace5ab39b0
lucie-godot <32601441+lucie-godot@users.noreply.github.com>
2017-11-20 14:32:47 +0100

    Update Random.cpp

--- source/Random.cpp
+++ source/Random.cpp
@@ -18,16 +18,16 @@ int Random::binomial (double prob, unsigned int pop)
 
 
 
-std::vector<double> Random::multinomial_pop (std::vector<double>* frequencies, unsigned int size_pop)
+std::vector<double> Random::multinomial_pop (std::vector<double> frequencies, unsigned int size_pop)
 {
 	std::vector<double> result;
 	unsigned int new_pop = size_pop;
 	unsigned int old_pop = size_pop;
 	
-	for (unsigned int i = 0; i < frequencies->size(); i += 1)
+	for (unsigned int i = 0; i < frequencies.size(); i += 1)
 	{
-		result.push_back((binomial(((*frequencies)[i]*size_pop)/old_pop, new_pop))/size_pop);
-		new_pop  -= (*frequencies)[i]*size_pop;
+		result.push_back((binomial(((frequencies)[i]*size_pop)/old_pop, new_pop))/size_pop);
+		new_pop  -= (frequencies)[i]*size_pop;
 		old_pop -= result[i];
 	}
 
