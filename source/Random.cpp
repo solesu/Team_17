8afb05139c62739c65c3c7829b08bbf029cfe7fe
lucie-godot <32601441+lucie-godot@users.noreply.github.com>
2017-11-20 13:26:44 +0100

    Update Random.cpp

--- source/Random.cpp
+++ source/Random.cpp
@@ -18,16 +18,16 @@ int Random::binomial (double prob, unsigned int pop)
 
 
 
-std::vector<double> Random::multinomial_pop (std::vector<double>* frequencies, unsigned int total_pop)
+std::vector<double> Random::multinomial_pop (std::vector<double>* frequencies, unsigned int size_pop)
 {
 	std::vector<double> result;
-	int new_pop = total_pop;
-	int old_pop = total_pop;
+	unsigned int new_pop = size_pop;
+	unsigned int old_pop = size_pop;
 	
 	for (unsigned int i = 0; i < frequencies->size(); i += 1)
 	{
-		result.push_back((binomial(((*frequencies)[i]*total_pop)/old_pop, new_pop))/total_pop);
-		new_pop  -= (*frequencies)[i]*total_pop;
+		result.push_back((binomial(((*frequencies)[i]*size_pop)/old_pop, new_pop))/size_pop);
+		new_pop  -= (*frequencies)[i]*size_pop;
 		old_pop -= result[i];
 	}
 
@@ -39,6 +39,6 @@ std::vector<double> Random::multinomial_pop (std::vector<double>* frequencies, u
 
 int Random::uniform(int value)
 {
-	std::uniform_int_distribution<int> uni (0, value);
+	std::uniform_int_distribution<unsigned int> uni (0, value);
 	return uni(gen);
 }
