35c453ec9c475e02427588bab6d041fb7b48856d
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-20 11:23:31 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -5,39 +5,7 @@
 
 //Questions 2)puis-je utiliser valeur avant d'avoir fait cmd.parse ou faire plusieurs fois
 
-using namespace TCLAP;
-using namespace std;
 
-Population* parse_args(int argc, char **argv ) 
-{
-	CmdLine cmd("Population parameters");
-	unsigned int option(0);
-	std::cout << "Would you like to choose the number or alleles (press 1) or to provide us with a fasta file (press 2)?" << std::endl;
-	std::cin>> option;
-			
-		if(option==1) ///Est-il possible d'utilise alors qu'on a pas fait cmd.parse ? -> sort du bloc if grrrr
-		{
-			ValueArg<unsigned int> population_size("p", "population_size", "Enter the population size: ", false, 1000, "unsigned int");
-			cmd.add(population_size);
-			ValueArg<unsigned int> generations_number("g", "generations_number", "Enter the number of generation: ", false, 2, "unsigned int");
-			cmd.add(generations_number);
-			ValueArg<unsigned int> alleles_number("a", "alleles_number", "Enter the number of different alleles: ", false, 2, "unsigned int");
-			cmd.add(alleles_number);
-			MultiArg<double> alleles_frequency("f", "alleles_frequency", "Enter the frequency of the alleles ", true, "double");
-			cmd.add(alleles_frequency);
-			
-			// Parse the argv arra
-			cmd.parse(argc, argv);
-		
-		Population* pop = new Population(population_size.getValue(),generations_number.getValue(), alleles_number.getValue(),alleles_frequency.getValue());
-		
-		return pop;
-		
-		} //else fasta format
-		
-		
-
-}
 
 int main(int argc, char ** argv)
 {
@@ -46,8 +14,8 @@ int main(int argc, char ** argv)
 		unsigned int runs_number;
 		std::cout << "How many times would you like to run the program with the same parameters?" << std::endl;
 		std::cin>> runs_number;
-		Population* pop(parse_args(argc, argv)); 
 		Simulation s;
+		Population* pop(s.parse_args(argc, argv)); 
 		s.launchSimulation(pop, runs_number);
 	
 	} 
