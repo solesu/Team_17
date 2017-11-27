4422324b6685cffcabc14dee2b29f22bca9d55a8
anoukross <32436342+anoukross@users.noreply.github.com>
2017-11-27 11:12:04 +0100

    Correction indentation

--- source/main.cpp
+++ source/main.cpp
@@ -1,11 +1,8 @@
  #include "Population.hpp"
  #include "Simulation.hpp"
- 
-//Questions 2)puis-je utiliser valeur avant d'avoir fait cmd.parse ou faire plusieurs fois
 
  int main(int argc, char ** argv)
  {
- 	
  	TCLAP::CmdLine cmd("Simulation parameters");
  	TCLAP::ValueArg<unsigned int> runs_number("r", "runs_number", "How many replicates would you like to create?", false, 0, "unsigned int") ;
  	cmd.add(runs_number);
@@ -17,9 +14,6 @@
  	Simulation s;
  	Population* pop(s.create_pop(generations_number.getValue(), marker_sites.getValue())); 
  	s.launchSimulation(pop, runs_number.getValue());
- 	
- 	 
- 	
- 	
- 		return 0;
+ 
+ 	return 0;
 }
